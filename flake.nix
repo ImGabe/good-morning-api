c2bc40bea138c93b5a32dd0ba2e04835c768dfb2
ImGabe <gabrielpmonte@hotmail.com>
2021-07-14 12:39:53 -0300

    first commit

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    naersk.url = "github:nmattia/naersk";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, naersk }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages."${system}";
+        naersk-lib = naersk.lib."${system}";
+      in
+        rec {
+          # `nix build`
+          packages.good-morning-api = naersk-lib.buildPackage {
+            pname = "good-morning-api";
+            root = ./.;
+          };
+          defaultPackage = packages.good-morning-api;
+
+          # `nix run`
+          apps.good-morning-api = flake-utils.lib.mkApp {
+            drv = packages.good-morning-api;
+          };
+          defaultApp = apps.good-morning-api;
+
+          # `nix develop`
+          devShell = pkgs.mkShell {
+            nativeBuildInputs = with pkgs; [ rustc cargo ];
+          };
+        }
+    );
+}
